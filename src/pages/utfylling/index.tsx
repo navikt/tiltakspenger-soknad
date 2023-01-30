66ee120d3f0e11853d061f537e65b67c2386d8dd
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-01-30 14:51:41 +0100

    Noe enkel global styling, noen flere spørsmål
    Også begynt å sette opp datovelger, med litt blandet hell
    
    Co-authored-by: thorellefsennav <Thor.Eirik.Ellefsen@nav.no>

--- src/pages/utfylling/index.tsx
+++ src/pages/utfylling/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import JaNeiSpørsmål from "@/components/ja-nei-spørsmål/JaNeiSpørsmål";
 import Flervalgsspørsmål from "@/components/flervalgsspørsmål/Flervalgsspørsmål";
+import {
+  GuidePanel,
+  UNSAFE_DatePicker,
+  UNSAFE_useRangeDatepicker,
+} from "@navikt/ds-react";
 
 export default function Utfylling() {
   const formMethods = useForm();
-  const { handleSubmit, watch } = formMethods;
+  const { handleSubmit, watch, control } = formMethods;
   const watchBorPåInstitusjon = watch("borPåInstitusjon");
   const onSubmit = console.log;
+  const { datepickerProps, toInputProps, fromInputProps } =
+    UNSAFE_useRangeDatepicker({});
 
   return (
     <FormProvider {...formMethods}>
@@ -33,6 +40,53 @@ export default function Utfylling() {
             Hvilken type institusjon bor du på?
           </Flervalgsspørsmål>
         )}
+        <GuidePanel poster>
+          Fyll ut informasjon om tiltaket du vil delta på i feltene under. Hvis
+          du ikke har avtalt tiltaket med NAV, vil vi kontakte deg for å avklare
+          om vi kan godkjenne tiltaket.
+        </GuidePanel>
+        <Flervalgsspørsmål
+          alternativer={[
+            { tekst: "AMO", value: "AMO" },
+            { tekst: "Arbeidstrening", value: "arbeidstrening" },
+            { tekst: "Jobbsøkerkurs", value: "jobbsøkerkurs" },
+            { tekst: "Annet type tiltak", value: "annet" },
+          ]}
+          name="tiltakstype"
+        >
+          Hvilken type tiltak søker du tiltakspenger for?
+        </Flervalgsspørsmål>
+
+        <Controller
+          control={control}
+          defaultValue={{ fra: "", til: "" }}
+          render={({ field: { onChange, value } }) => (
+            <UNSAFE_DatePicker
+              {...datepickerProps}
+              onSelect={(periode: any) => {
+                onChange({ fra: periode.from || "", til: periode.til || "" });
+              }}
+            >
+              <div className="flex flex-wrap justify-center gap-4">
+                <UNSAFE_DatePicker.Input
+                  {...fromInputProps}
+                  label="Fra"
+                  value={value.fra}
+                  onChange={(fra) => {
+                    onChange({ fra, til: value.til });
+                  }}
+                />
+                <UNSAFE_DatePicker.Input
+                  {...toInputProps}
+                  label="Til"
+                  value={value.til}
+                  onChange={(til) => onChange({ til, fra: value.fra })}
+                />
+              </div>
+            </UNSAFE_DatePicker>
+          )}
+          name="tiltaksperiode"
+        ></Controller>
       </form>
     </FormProvider>
   );
