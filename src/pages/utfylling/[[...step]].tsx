1b0164fa51252213e90e03f42a9c2f04651cba6c
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2023-02-08 15:21:19 +0100

    Demo av stepper fra Aksel

--- src/pages/utfylling/[[...step]].tsx
+++ src/pages/utfylling/[[...step]].tsx
@@ -3,6 +3,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { useRouter } from 'next/router';
 import Oppsummeringssteg from '@/components/oppsummeringssteg/Oppsummeringssteg';
 import Utfyllingssteg from '@/components/utfyllingssteg/Utfyllingssteg';
+import {Heading, Stepper} from "@navikt/ds-react";
 
 export default function Utfylling() {
     const router = useRouter();
@@ -18,20 +19,38 @@ export default function Utfylling() {
         },
     });
 
+    const resolveStepNumber = (stepString: any) => {
+        switch (stepString ? stepString[0] : "") {
+            case 'utfylling':
+                return 1;
+            case 'oppsummering':
+                return 2;
+            default:
+                return 1;
+        }
+    }
     const navigerBrukerTilOppsummeringssteg = () => router.push('/utfylling/oppsummering');
     const sendSøknad = (data: any) => console.log(data);
 
-    if (step && step[0] === 'oppsummering') {
-        return (
+    return (
+        <>
+            <Heading size="medium" spacing level="2" id="stepper-heading">
+                Søknadssteg
+            </Heading>
+            <Stepper
+                aria-labelledby="stepper-heading"
+                activeStep={resolveStepNumber(step)}
+                orientation="horizontal"
+            >
+                <Stepper.Step as="button" onClick={() => router.push('/utfylling/utfylling')}>Utfylling</Stepper.Step>
+                <Stepper.Step as="button" onClick={() => router.push('/utfylling/oppsummering')}>Oppsummering</Stepper.Step>
+            </Stepper>
             <FormProvider {...formMethods}>
-                <Oppsummeringssteg onCompleted={sendSøknad} />
+                {(step && step[0] === 'oppsummering') ?
+                        <Oppsummeringssteg onCompleted={sendSøknad} /> :
+                        <Utfyllingssteg onCompleted={navigerBrukerTilOppsummeringssteg} />
+                }
             </FormProvider>
-        );
-    } else {
-        return (
-            <FormProvider {...formMethods}>
-                <Utfyllingssteg onCompleted={navigerBrukerTilOppsummeringssteg} />
-            </FormProvider>
-        );
-    }
+        </>
+    )
 }
