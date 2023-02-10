be1afcdcd378781e82f982d315ebd30ec6b0d47b
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-02-10 14:57:18 +0100

    Lagde introside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import React, { MouseEvent } from 'react';
-import { Button } from '@navikt/ds-react';
+import { Button, ConfirmationPanel, GuidePanel } from '@navikt/ds-react';
 import { useRouter } from 'next/router';
+import styles from './index.module.css';
 
 export default function App() {
     const router = useRouter();
+
+    const [markerBekreftelsesboksSomRød, setMarkerBekreftelsesboksSomRød] = React.useState(false);
+    const [brukerHarBekreftet, setBrukerHarBekreftet] = React.useState(false);
+
+    React.useEffect(() => {
+        if (brukerHarBekreftet && markerBekreftelsesboksSomRød) {
+            setMarkerBekreftelsesboksSomRød(false);
+        }
+    }, [brukerHarBekreftet]);
 
     const startSøknad = (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
@@ -11,8 +21,42 @@ export default function App() {
     };
 
     return (
-        <Button onClick={startSøknad} type="button">
-            Start søknad om tiltakspenger
-        </Button>
+        <div>
+            <h2 className={styles.søknadstittel}>Søknad om tiltakspenger</h2>
+            <GuidePanel poster>Hei! Jeg er her for å veilede deg gjennom søknaden om tiltakspenger.</GuidePanel>
+            <ConfirmationPanel
+                className={styles.bekreftelsespanel}
+                label="Jeg vil svare så godt jeg kan på spørsmålene i søknaden"
+                value={brukerHarBekreftet}
+                onChange={() => setBrukerHarBekreftet(!brukerHarBekreftet)}
+                error={
+                    markerBekreftelsesboksSomRød
+                        ? 'Du må bekrefte at du vil svare så godt du kan på spørsmålene i søknaden'
+                        : null
+                }
+            >
+                <b>Vi stoler på deg</b>
+            </ConfirmationPanel>
+            <div className={styles.knappeseksjon}>
+                <Button
+                    onClick={() => console.log('Hva skal vi gjøre når bruker avbryter søknaden?')}
+                    type="button"
+                    size="small"
+                    variant="secondary"
+                >
+                    Avbryt søknaden
+                </Button>
+                <Button
+                    onClick={(event) => {
+                        if (brukerHarBekreftet) startSøknad(event);
+                        else setMarkerBekreftelsesboksSomRød(true);
+                    }}
+                    type="button"
+                    size="small"
+                >
+                    Start søknaden
+                </Button>
+            </div>
+        </div>
     );
 }
