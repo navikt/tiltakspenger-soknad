1657890fe98ca9cc6fd429d4aab313cd02da276d
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-05-04 10:39:18 +0200

    Setup faro for client side logging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app';
 import '@navikt/ds-css';
 import '../styles/global.css';
-import {useState} from "react";
-import {v4 as uuidv4} from "uuid";
-import {Personalia} from "@/types/Personalia";
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Personalia } from '@/types/Personalia';
+import { initializeFaro } from '@grafana/faro-web-sdk';
 
 export const defaultPersonalia = {
     fornavn: 'Foo',
@@ -13,11 +14,19 @@ export const defaultPersonalia = {
     barn: [{ fornavn: 'Test', etternavn: 'Testesen', fødselsdato: '2025-01-01', uuid: uuidv4() }],
 };
 
+if (typeof window !== 'undefined') {
+    initializeFaro({
+        url: process.env.NEXT_PUBLIC_TELEMETRY_URL,
+        app: {
+            name: 'tiltakspenger-soknad',
+        },
+    });
+}
+
 function App({ Component, pageProps }: AppProps) {
     const [personaliaData, setPersonaliaData] = useState<Personalia>(defaultPersonalia);
 
-    return (<Component {...pageProps} setPersonaliaData={setPersonaliaData} personalia={personaliaData}/>);
+    return <Component {...pageProps} setPersonaliaData={setPersonaliaData} personalia={personaliaData} />;
 }
 
 export default App;
-
