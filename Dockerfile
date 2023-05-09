664d5f2aa70424c7a95231ab455487dda8347dc7
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-05-09 09:53:14 +0200

    Bygge med environment-fil fra Next
    Fjernet utkommentering av Faro-oppsett
    
    Co-authored-by: thorellefsennav <Thor.Eirik.Ellefsen@nav.no>
    Co-authored-by: KarlEvaldNav <Karl.Evald.Wigestrand@nav.no>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:16-alpine as runtime
 
+ARG environment
+
 # Check https://github.com/nodejs/docker-node/tree/b4117f9333da4138b03a546ec926ef50a31506c3#nodealpine to understand why libc6-compat might be needed.
 RUN apk add --no-cache libc6-compat
 
@@ -9,6 +11,7 @@ COPY package.json .
 COPY next.config.js .
 COPY .next/standalone ./
 COPY .next/static ./.next/static
+COPY .env.${environment:?} .env.production
 
 ENV NEXT_TELEMETRY_DISABLED 1
 ENV PORT 3000
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/global.css';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { Personalia } from '@/types/Personalia';
+import { initializeFaro } from '@grafana/faro-web-sdk';
 
 export const defaultPersonalia = {
     fornavn: 'Foo',
@@ -13,19 +14,18 @@ export const defaultPersonalia = {
     barn: [{ fornavn: 'Test', etternavn: 'Testesen', fødselsdato: '2025-01-01', uuid: uuidv4() }],
 };
 
-// TODO: Vi mangler å få satt miljøvariabelen her riktig
-// if (typeof window !== 'undefined') {
-//     initializeFaro({
-//         url: process.env.NEXT_PUBLIC_TELEMETRY_URL,
-//         app: {
-//             name: 'tiltakspenger-soknad',
-//         },
-//     });
-// }
+if (typeof window !== 'undefined') {
+    initializeFaro({
+        url: process.env.NEXT_PUBLIC_TELEMETRY_URL,
+        app: {
+            name: 'tiltakspenger-soknad',
+        },
+    });
+}
 
 function App({ Component, pageProps }: AppProps) {
     const [personaliaData, setPersonaliaData] = useState<Personalia>(defaultPersonalia);
-    // console.error('Test av feilmelding');
+    console.error('Test av feilmelding');
     return <Component {...pageProps} setPersonaliaData={setPersonaliaData} personalia={personaliaData} />;
 }
 
