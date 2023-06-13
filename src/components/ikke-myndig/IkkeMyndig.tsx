6dd6a0668d8ebb934726f0a67774d5efd91cf213
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-06-13 15:27:48 +0200

    Tekstjusteringer IkkeMyndig

--- src/components/ikke-myndig/IkkeMyndig.tsx
+++ src/components/ikke-myndig/IkkeMyndig.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { GuidePanel, Heading, Link } from '@navikt/ds-react';
+import { BodyLong, GuidePanel, Heading, Link } from '@navikt/ds-react';
 import { UtfyllingContext } from '@/pages/utfylling/[[...step]]';
 import styles from './IkkeMyndig.module.css';
 
@@ -11,21 +11,24 @@ export default function IkkeMyndig() {
                 Søknad om tiltakspenger
             </Heading>
             <GuidePanel poster className={styles.ikkeMyndigGuidePanel}>
-                <p>
+                <BodyLong spacing>
                     Hei,{' '}
                     {`${personalia?.fornavn} ${personalia?.mellomnavn ? `${personalia?.mellomnavn} ` : ''}${
                         personalia?.etternavn
                     }`}
-                </p>
-                <p>
+                </BodyLong>
+                <BodyLong spacing>
                     For å ha rett til tiltakspenger må du være fylt 18 år. Du må også være fylt 18 år for å levere
                     digital søknad om tiltakspenger.
-                </p>
-                <p>
-                    Om du ønsker å søke tiltakspenger før du fylt 18 år, må du få underskrift av en foresatt eller verge
-                    og sende søknad med vanlig post. Du finner papirsøknaden som du kan skrive ut ved å trykke på
-                    knappen under merket "Send søknad med post".
-                </p>
+                </BodyLong>
+                <BodyLong spacing>
+                    Hvis du ønsker å søke tiltakspenger før du fyller 18 år, må du få underskrift av en foresatt eller
+                    verge og sende søknad med vanlig post.
+                </BodyLong>
+                <BodyLong>
+                    Hvis du skal sende søknaden i posten kan du følge lenken "Send søknad med post". Her vil du få
+                    instruksjoner for hvordan du skal gå fram.
+                </BodyLong>
             </GuidePanel>
             <div className={styles.sendSøknadMedPostLink}>
                 <Link href="https://www.nav.no/soknader/nb/person/arbeid/tiltakspenger">Send søknad med post</Link>
