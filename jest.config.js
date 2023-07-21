154f56a2d2ed0fd23c657b8df7813e997dbe5055
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-07-21 10:52:35 +0200

    Fikset test-oppsett som ødela for next build
    * Flyttet alt av tester under /pages til __tests__ på rot
    * Renamet babel.config til babel.test.config og sørger for at den kun
      blir brukt i testsammenheng (ellers brekker det next build)

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
         '^.+\\.tsx?$': [
             'ts-jest',
             {
-                babelConfig: true,
+                babelConfig: '<rootDir>/babel.test.config.js',
             },
         ],
     },
