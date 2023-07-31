77d060bbe7d4b038e2693a7830b747e9612022ff
Erlend Vige <erlend.edvardsen.vige@nav.no>
2023-07-31 11:07:55 +0200

    Sørge for at vi alltid har oppdatert info fra Unleash
    Det var en feil i koden som gjorde at vi kun hentet data fra Unleash ved
    oppstart av appen. Dette er nå fikset ved at vi gjør kall mot Unleash
    hver gang featureIsEnabled() kalles på.
    
    Co-authored-by: KarlEvaldNav <Karl.Evald.Wigestrand@nav.no>

--- src/utils/featureToggling.ts
+++ src/utils/featureToggling.ts
@@ -1,30 +1,9 @@
 import { evaluateFlags, flagsClient, getDefinitions, randomSessionId } from '@unleash/nextjs';
-import logger from '@/utils/serverLogger';
-import { ClientFeaturesResponse } from 'unleash-client';
-
-let definitions: ClientFeaturesResponse | null = null;
-
-async function setupUnleash() {
-    logger.info('Starter oppsett av unleash');
-    definitions = await getDefinitions();
-    logger.info('Oppsett av unleash OK');
-}
-
-function getFlags() {
-    const sessionId = randomSessionId();
-    const { toggles } = evaluateFlags(definitions!!, { sessionId });
-    const flags = flagsClient(toggles);
-    return flags;
-}
 
 export async function featureIsEnabled(featureName: string) {
-    if (definitions == null) {
-        try {
-            await setupUnleash();
-        } catch (e) {
-            throw e;
-        }
-    }
-    const flags = getFlags();
+    const sessionId = randomSessionId();
+    const definitions = await getDefinitions();
+    const { toggles } = evaluateFlags(definitions!!, { sessionId });
+    const flags = flagsClient(toggles);
     return flags.isEnabled(featureName);
 }
